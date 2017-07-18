8798fffa4fdaf41daec4e262ec380ebadfc0904f
WJ Yang <yangwj116@qq.com>
2017-07-18 20:20:30 +0800

    another way to to write CMakeLists

---
+++ openGL/src/1.Getting_Started/TriangleApp.cpp
@@ -0,0 +1,113 @@
+#include "glad/glad.h"
+#include "PointApp.h"
+#include "Utils.h"
+
+#include <cmath>
+
+#define glCheckError() util::glCheckError_(__FILE__, __LINE__) 
+#define SIZE 3
+#define ELEMENT_NUMBER 1
+
+void PointApp::startup() {
+  Application::startup();
+
+  // do nothing
+  initShader();
+}
+
+void PointApp::run(double timeMills) {
+  // ===== render commands =====
+
+  // set color
+  glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+  // glCheckError();
+  // we only care about color values now
+  glClear(GL_COLOR_BUFFER_BIT);
+  
+  // glCheckError();
+  glUseProgram(mProgram);
+  // glCheckError();
+
+  const int START = 0;
+  glDrawArrays(GL_TRIANGLES, START, SIZE);
+  
+}
+
+void PointApp::shutdown() {
+  glDeleteProgram(mProgram);
+  glDeleteVertexArrays(SIZE, &mVAO);
+}
+
+void PointApp::initShader() {
+  static const char * vertexStr[] = 
+  {
+    "#version 420 core                                 \n"
+    "                                                  \n"
+    "out vec4 out_color;                               \n"
+    "                                                  \n"
+    "void main(void)                                   \n"
+    "{                                                 \n"
+    "    const vec4 vertices[3] = vec4[3](             \n"
+    "        vec4(0.2f, 0.2f, 0.0f, 1.0f),             \n"
+    "        vec4(-0.2f, -0.2f, 0.0f, 1.0f),           \n"
+    "        vec4(0.2f, -0.2f, 0.0f, 1.0f)             \n"
+    "    );                                            \n"
+    "    const vec4 colors[3] = vec4[3](               \n"
+    "        vec4(0.2f, 0.2f, 0.2f, 1.0f),             \n"
+    "        vec4(0.5f, 0.5f, 0.5f, 1.0f),             \n"
+    "        vec4(0.8f, 0.8f, 0.8f, 1.0f)              \n"
+    "    );                                            \n"
+    "    gl_Position = vertices[gl_VertexID];          \n"
+    "    out_color = colors[gl_VertexID];              \n"
+  };
+  static const char * fragStr[] = 
+  {
+      "#version 420 core                             \n"
+      "                                              \n"
+      "out vec4 color;                               \n"
+      "in vec4 out_color;                            \n"
+      "                                              \n"
+      "void main(void)                               \n"
+      "{                                             \n"
+      "    color = out_color;    \n"
+      "}                                             \n"
+  };
+
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, ELEMENT_NUMBER, vertexStr, nullptr);
+  glCompileShader(vertexShader);
+  glCheckError();
+  
+  GLuint fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragShader, ELEMENT_NUMBER, fragStr, nullptr);
+  glCompileShader(fragShader);
+
+  mProgram = glCreateProgram();
+  glAttachShader(mProgram, vertexShader);
+  glAttachShader(mProgram, fragShader);
+  glCheckError();
+
+  glLinkProgram(mProgram);
+  
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragShader);
+
+  glGenVertexArrays(SIZE, &mVAO);
+  glBindVertexArray(mVAO);
+  glCheckError();
+}
+
+  // vertex
+  // #version 330 core
+
+  // void main(void) {
+  //   gl_Position = vec4(0.0, 0.0, 0.5, 1.0);
+  // }
+
+  // frag
+  // #version 330 core 
+  // out vec4 color;
+
+  // void main(void) {
+  //   color = vec4(0.0, 0.5, 0.5, 1.0);
+  // }
--- openGL/src/Main.cpp
+++ openGL/src/Main.cpp
@@ -3,10 +3,66 @@
 #include "GLFW/glfw3.h"
 
 #include <cstdio>
+#include <iostream>
 
 // called each time  the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
   glViewport(0, 0, width, height);
+}
+
+// void APIENTRY glDebugOutput(GLenum source, 
+//                             GLenum type, 
+//                             GLuint id, 
+//                             GLenum severity, 
+//                             GLsizei length, 
+//                             const GLchar *message, 
+//                             void *userParam)
+// {
+void APIENTRY glDebugOutput(unsigned int source, 
+                            unsigned int type, 
+                            unsigned int id, 
+                            unsigned int severity, 
+                            int length, 
+                            const char *message, 
+                            const void *userParam)
+{
+    // 忽略一些不重要的错误/警告代码
+    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return; 
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break; 
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
 }
 
 void processInput(GLFWwindow* window) {
@@ -28,6 +84,23 @@ int main() {
 
   // only use core openGL features
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // set for debug(is slow)
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+  // glDebugMessageControl(GL_DEBUG_SOURCE_API, 
+  //                     GL_DEBUG_TYPE_ERROR, 
+  //                     GL_DEBUG_SEVERITY_HIGH,
+  //                     0, nullptr, GL_TRUE); 
+  // GLint flags;
+  // glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+  // if(flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
+  //   printf("the debug has been detected");
+  //   glEnable(GL_DEBUG_OUTPUT);
+  //   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
+  //   glDebugMessageCallback(glDebugOutput, nullptr);
+  //   glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+  // }
+
 
   GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", nullptr, nullptr);
   if(window == nullptr) {
